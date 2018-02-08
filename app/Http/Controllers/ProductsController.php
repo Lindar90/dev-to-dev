2b07c04df4137fb8b32a647b2f434c1d67df20ac
Denis Marusevich <denismarusevich@gmil.com>
2018-02-08 22:58:50 +0300

    Pipeline lesson: before refactoring

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,9 +14,21 @@ class ProductsController extends Controller
 
     public function products(Request $request)
     {
-        $filters = $request->input('filters', []);
+        $products = Product::latest();
 
-        $products = Product::all();
+        if ($brand = $request->input('filters.brands')) {
+            $products->where('brand', $brand);
+        }
+
+        if ($size = $request->input('filters.sizes')) {
+            $products->where('size', $size);
+        }
+
+        if ($color = $request->input('filters.colors')) {
+            $products->where('color', $color);
+        }
+
+        $products = $products->get();
 
         return [
             'filters'  => [
